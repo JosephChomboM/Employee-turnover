4cf138a35386239603527c56ba969aa296a555ac
Joseph Chombo Morocho <josephalexander0704@gmail.com>
2024-01-31 23:26:41 -0500

    training

---
+++ src/models/train.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+from sklearn.preprocessing import OneHotEncoder
+
+
+df = pd.read_csv('../../data/processed/trainAbandonoE.csv', index_col='id')
+df
+
+df_ml = df.copy()
+df_ml.info()
+
+# Categorical
+cat = df_ml.select_dtypes('O')
+
+# Instance
+ohe = OneHotEncoder(sparse = False)
+
+# Training
+ohe.fit(cat)
+
+cat_ohe = ohe.transform(cat)
+
+cat_ohe = pd.DataFrame(
+    cat_ohe,
+    columns = ohe.get_feature_names_out(input_features = cat.columns)
+).reset_index(drop = True)
+
+cat_ohe
+
+num = df.select_dtypes('number').reset_index(drop = True)
+
+df_ml = pd.concat([cat_ohe, num], axis = 1)
+df_ml
+
+
